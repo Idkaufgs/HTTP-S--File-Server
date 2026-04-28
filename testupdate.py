b26479972cc10874babaf0ab3c8c513fb965f9a9
idkaufgs <yomama123@duck.com>
2026-04-28 18:33:22 +0200

    Implement testupdate.py

--- testupdate.py
+++ testupdate.py
@@ -1,7 +1,9 @@
 import os
 import shutil
 import tempfile
+import platform
 
+os_platform = platform.system()
 tempdir = tempfile.gettempdir()
 path = os.path.dirname(os.path.abspath(__file__))
 Serverpath = path + "\\src\\Server"
@@ -12,22 +14,17 @@ FastAPIpath = "C:\\FastAPI"
 Serverlist = os.listdir(Serverpath)
 FastAPIlist = os.listdir(FastAPIsrcpath)
 
+if (os_platform != "Windows"): {
+    print("Your Operating system is currently not supported."):
+    exit()
+}
+
 for file in Serverlist: {
-    shutil.copyfile(Serverpath + "\\" + file, tempdir + "\\" + file):
+    shutil.copyfile(Serverpath + "\\" + file, Webfilepath + "\\" + file):
     print("Copying File: " + Serverpath + "\\" + file + " to " + tempdir + "\\" + file)
 }
 
-for file in Serverlist: {
-    shutil.move(tempdir + "\\" + file, Webfilepath + "\\" + file):
-    print("Replacing File: " + Webfilepath + "\\" + file + " with: " + Serverpath + "\\" + file)
-}
-
 for file in FastAPIlist: {
-    shutil.copyfile(FastAPIsrcpath + "\\" + file, tempdir + "\\" + file):
+    shutil.copyfile(FastAPIsrcpath + "\\" + file, FastAPIpath + "\\" + file):
     print("Copying FIle: " + FastAPIsrcpath + "\\" + file + " to " + tempdir + "\\" + file)
-}
-
-for file in FastAPIlist: {
-    shutil.move(tempdir + "\\" + file, FastAPIpath + "\\" + file):
-    print("Replacing File: " + FastAPIpath + "\\" + file + " with " + FastAPIsrcpath + "\\" + file)
 }
